1a3bcf99fa065a1f14a3c76aa34a34d06f905bbb
Thin Myat Naing <52567884+Thin-Myat-Naing@users.noreply.github.com>
2026-09-04 15:22:15 +0630

    update data files

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ app = Flask(__name__)
 # DATABASE (MySQL Setup)
 # ==========================================================
 
-DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_USER = os.environ.get("DB_USER", "root")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "root")
-DB_NAME = os.environ.get("DB_NAME", "Cloud-Twitter Sentiment")
+DB_HOST = os.environ["DB_HOST"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_NAME = os.environ["DB_NAME"]
 DB_PORT = int(os.environ.get("DB_PORT", 3306))
 
 
